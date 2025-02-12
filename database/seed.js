99e1b51702d468f36aca35114bcc35122f916b2d
vdekterev <vlad.decterev@icloud.com>
2025-02-12 21:18:01 +0400

    Supabase | done

--- database/seed.js
+++ database/seed.js
@@ -1,10 +1,49 @@
 /* eslint-env node */
 
-import { fakerEN_US as faker } from '@faker-js/faker';
-
+import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
-  process.env.SERVICE_ROLE_KEY,
-)
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`
+  )
+  process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
+const seedProjects = async (numEntries) => {
+  logStep('Seeding projects...')
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+
+    projects.push({
+      name: name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
+    })
+  }
+
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+  if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
+}
+
+const seedDatabase = async (numEntriesPerTable) => {
+  await seedProjects(numEntriesPerTable)
+}
+
+const numEntriesPerTable = 10
+
+await seedDatabase(numEntriesPerTable)
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { supabase } from "@/lib/supabaseClient.ts";
+import { ref } from "vue";
+
+const projects = ref<[]>([]);
+
+(async () => {
+  const {data, error} = await supabase.from('projects').select();
+  if (error) console.error(error);
+  projects.value = data;
+})();
+
+
 </script>
 
 <template>
   <main>
     <h1>Welcome Projects</h1>
+    <div>
+      <ul>
+        <li v-for="p in projects" :key="p.id">{{ p.name }}</li>
+      </ul>
+    </div>
     <RouterLink :to="{ name: '/' }">
       Home
     </RouterLink>
