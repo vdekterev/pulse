3884f2c1f075a9774bc9fced45c0be5f2231e7ea
vdekterev <vlad.decterev@icloud.com>
2025-02-12 20:34:55 +0400

    Supabase | commit

---
+++ database/seed.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+
+import { fakerEN_US as faker } from '@faker-js/faker';
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL,
+  process.env.SERVICE_ROLE_KEY,
+)
