afc4062f1d41a59075a08356a3f7c2fbafb036df
vdekterev <vlad.decterev@icloud.com>
2025-02-15 15:52:11 +0400

    not yet...

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -3,23 +3,23 @@ import { supabase } from "@/lib/supabaseClient.ts";
 import { ref } from "vue";
 import type { Tables } from "../../../database/types.ts";
 
-const projects = ref<Tables<'projects'>[] | null>(null);
+const tasks = ref<Tables<'tasks'>[] | null>(null);
 
 (async () => {
-  const {data, error} = await supabase.from('projects').select();
+  const {data, error} = await supabase.from('tasks').select();
   if (error) console.error(error);
-  projects.value = data;
+  tasks.value = data;
 })();
 
 </script>
 
 <template>
   <main>
-    <h1>Welcome Projects</h1>
+    <h1>Welcome Tasks</h1>
     <div>
       <ul>
-        <li v-for="p in projects" :key="p.id">
-          {{ p.name }}
+        <li v-for="t in tasks" :key="t.id">
+          {{ t.name }}
         </li>
       </ul>
     </div>
