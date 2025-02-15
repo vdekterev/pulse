430fb3802e4659fd59dcd1a7e258bc2cbbbafaa7
vdekterev <vlad.decterev@icloud.com>
2025-02-15 17:51:43 +0400

    dark theme and nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-import { supabase } from '@/lib/supabaseClient';
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+    <form class="w-full max-w-96">
+      <input type="text" placeholder="Search..." class="pl-8 w-full bg-background rounded">
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView/>
   </main>
